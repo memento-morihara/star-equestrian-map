110e16205eba072c523f9ef9ce561c2f52825af4
memento-morihara <mementomorihara@gmail.com>
2023-07-20 14:48:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Star Equestrian Map (WIP)
+
+<img src="assets/StarEquestrianLogo_Color.png" alt="Star Equestrian game logo" width="300" />
+
+
+An interactive map for [Star Equestrian](https://www.foxieventures.com/star-equestrian/).
+
+Very much a work in progress, but maybe someone will find it useful.
+
+Not too much to say about it yet. Click on the icons to open a popup that might have more information about the item's location. Use the checkboxes in the side menu to show/hide icons.
+
+## To Do
+
+- [ ] Finish basemap and patch various defects
+- [ ] Fix weird popup placement and icon sizing
+- [ ] Different icons for chests
+- [ ] Add ability to mark items as collected
+- [ ] Finish adding locations
+- [ ] UI improvements (side menu, popups, etc.)
+- [ ] Accessibility
+- [ ] Mobile support
+- [ ] Marker visibility
+
+---
+
+This is an unofficial fan-made project. Game, logo, and all imagery &copy; [Foxie Ventures](https://www.foxieventures.com).
+
+**Credits:**
+
+- [MapTiler Engine](https://www.maptiler.com/engine/)
+- [Leaflet](https://leafletjs.com)
+- [Leaflet.contextmenu](https://github.com/aratcliffe/Leaflet.contextmenu)
+- [Leaflet.SidePanel](https://github.com/maxwell-ilai/Leaflet.SidePanel)
---
+++ markers.js
@@ -0,0 +1,224 @@
+export const food = [
+	{
+		name: "Apple",
+		icon: L.icon({
+			iconUrl: "./icons/webp/apple.webp",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Berries",
+		icon: L.icon({
+			iconUrl: "./icons/berries.png",
+			iconSize: [ 22, 22 ]
+		})
+	},
+	{
+		name: "Brown Mushroom",
+		icon: L.icon({
+			iconUrl: "./icons/webp/brown-mushroom.webp",
+			iconSize: [ 20, 20 ]
+		})
+	},
+	{
+		name: "Carrot",
+		icon: L.icon({
+			iconUrl: "./icons/webp/carrot.webp",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Honey",
+		icon: L.icon({
+			iconUrl: "./icons/webp/honey.webp",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Lemon",
+		icon: L.icon({
+			iconUrl: "./icons/webp/lemon.webp",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Lettuce",
+		icon: L.icon({
+			iconUrl: "./icons/webp/lettuce.webp",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Orange",
+		icon: L.icon({
+			iconUrl: "./icons/orange.png",
+			iconSize: [ 16, 16 ]
+		})
+	},
+	{
+		name: "Pumpkin",
+		icon: L.icon({
+			iconUrl: "./icons/pumpkin.png",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Strawberries",
+		icon: L.icon({
+			iconUrl: "./icons/strawberry.png",
+			iconSize: [ 24, 24 ]
+		})
+	},
+	{
+		name: "Truffle",
+		icon: L.icon({
+			iconUrl: "./icons/truffle.png",
+			iconSize: [ 22, 22 ]
+		})
+	},
+	{
+		name: "Turnip",
+		icon: L.icon({
+			iconUrl: "./icons/turnip.png",
+			iconSize: [ 28, 28 ]
+		})
+	},
+	{
+		name: "Watermelon",
+		icon: L.icon({
+			iconUrl: "./icons/watermelon.png",
+			iconSize: [ 20, 20 ]
+		})
+	},
+	{
+		name: "Wheat",
+		icon: L.icon({
+			iconUrl: "./icons/wheat.png",
+			iconSize: [ 28, 28 ]
+		})
+	},
+	{
+		name: "White Mushroom",
+		icon: L.icon({
+			iconUrl: "./icons/white-mushroom.png",
+			iconSize: [ 20, 20 ]
+		})
+	},
+]
+
+export const resources = [
+	{
+		name: "Butterflies",
+		icon: L.icon({
+			iconUrl: "./icons/butterfly.png",
+			iconSize: [ 20, 20 ]
+		})
+	},
+	{
+		name: "Eggs",
+		icon: L.icon({
+			iconUrl: "./icons/eggs.png",
+			iconSize: [ 16, 16 ]
+		})
+	},
+	{
+		name: "Fish",
+		icon: L.icon({
+			iconUrl: "./icons/fish.png",
+			iconSize: [ 20, 20 ]
+		})
+	},
+	{
+		name: "Milk",
+		icon: L.icon({
+			iconUrl: "./icons/milk.png",
+			iconSize: [ 20, 20 ]
+		})
+	},
+	{
+		name: "Wool",
+		icon: L.icon({
+			iconUrl: "./icons/wool.png",
+			iconSize: [ 18, 18 ]
+		})
+	},
+]
+
+export const chests = [
+	{
+		name: "Common Chest",
+		icon: L.icon({
+			iconUrl: "./se-marker.svg",
+			iconSize: [ 28, 28 ]
+		})
+	},
+	{
+		name: "Uncommon Chest",
+		icon: L.icon({
+			iconUrl: "./se-marker.svg",
+			iconSize: [ 28, 28 ]
+		})
+	},
+	{
+		name: "Rare Chest",
+		icon: L.icon({
+			iconUrl: "./se-marker.svg",
+			iconSize: [ 28, 28 ]
+		})
+	},
+	{
+		name: "Epic Chest",
+		icon: L.icon({
+			iconUrl: "./se-marker.svg",
+			iconSize: [ 28, 28 ]
+		})
+	},
+	{
+		name: "Legendary Chest",
+		icon: L.icon({
+			iconUrl: "./se-marker.svg",
+			iconSize: [ 28, 28 ]
+		})
+	},
+]
+
+export const collectibles = [
+	{
+		name: "Sheriff's Badge",
+		icon: L.icon({
+			iconUrl: "./icons/sheriffs-badge.png",
+			iconSize: [ 24, 24 ]
+		})
+	},
+	{
+		name: "Horseshoe",
+		icon: L.icon({
+			iconUrl: "./icons/horseshoe.png",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Toy Unicorn",
+		icon: L.icon({
+			iconUrl: "./icons/toy-unicorn.png",
+			iconSize: [ 24, 24 ]
+		})
+	},
+]
+
+export const other = [
+	{
+		name: "Picnic Basket",
+		icon: L.icon({
+			iconUrl: "./icons/picnic-basket.png",
+			iconSize: [ 18, 18 ]
+		})
+	},
+	{
+		name: "Wood",
+		icon: L.icon({
+			iconUrl: "./icons/wood.png",
+			iconSize: [ 22, 22 ]
+		})
+	},
+]
