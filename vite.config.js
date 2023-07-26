b213354e452b8c85ffacee7b7973ee2e0cdf00f4
memento-morihara <mementomorihara@gmail.com>
2023-07-25 23:27:14 -0700

    Temp fix for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
+        dir: 'docs',
         emptyOutDir: true,
         target: 'esnext',
         base: '/star-equestrian-map/',
