ed1aa7d7bcbfcbf352b15debada661dc75007b60
memento-morihara <mementomorihara@gmail.com>
2023-07-27 18:17:27 -0700

    Support filtering by category

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 import { atom } from 'nanostores'
 
 const checkboxes = document.querySelectorAll("input[type=checkbox]")
-const items = [ 'apple', 'berries', 'carrot', 'brown-mushroom', 'honey', 'lemon', 'lettuce', 'orange', 'pumpkin', 'truffle', 'strawberries', 'turnip', 'watermelon', 'wheat', 'white-mushroom', 'common-chest', 'uncommon-chest', 'rare-chest', 'epic-chest', 'legendary-chest', 'butterflies', 'eggs', 'fish', 'milk', 'wood', 'wool', 'horseshoe', "message-in-a-bottle", "sheriff's-badge", 'toy-unicorn', 'picnic-basket', 'cave-entrance' ]
+export const items = [ [ 'apple', 'berries', 'carrot', 'brown-mushroom', 'honey', 'lemon', 'lettuce', 'orange', 'pumpkin', 'truffle', 'strawberries', 'turnip', 'watermelon', 'wheat', 'white-mushroom' ], [ 'common-chest', 'uncommon-chest', 'rare-chest', 'epic-chest', 'legendary-chest' ], [ 'butterflies', 'eggs', 'fish', 'milk', 'wood', 'wool' ], [ 'horseshoe', "message-in-a-bottle", "sheriff's-badge", 'toy-unicorn' ], [ 'picnic-basket', 'wood', 'cave-entrance' ] ]
 export const shownItems = atom(localStorage.getItem('shownItems') ? localStorage.getItem('shownItems').split(',') : items);
 
 
