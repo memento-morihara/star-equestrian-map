ea986f9f5e440526d225767d6f2342fb6b4f02bb
memento-morihara <mementomorihara@gmail.com>
2023-10-11 22:42:41 -0700

    Load marker data from local files

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,16 +1,21 @@
 import PocketBase from 'pocketbase';
 
 const db = new PocketBase("https://panicky-potato.pockethost.io");
+let data = {};
 
 export async function load({ fetch, url }) {
     let searchId = null;
     if (new URLSearchParams(url.search).get("id")) {
         searchId = new URLSearchParams(url.search).get("id")
     }
+
+    for (const category of ["chest", "collectible", "food", "npc", "other", "resource", "tack"]) {
+      data[category] = await fetch(`data-${category}.json`).then(res => res.json());
+    }
+
     return {
         searchId: searchId,
-        data: fetch("/data.json").then(res => res.json()),
-        locations: await db.collection("list_new").getFullList(),
+        locationsNew: data,
         counts: await db.collection("count").getFullList(),
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,21 @@
     const initialLocation = data.searchId
         ? data.locations.find((location) => location.id === data.searchId)
         : null;
+
+    const locations = Object.values(data.locationsNew);
 </script>
 
 <Sidebar {data} tabs={["Filters", "Progress", "Settings"]} />
 <main>
     <Map initialLocation={initialLocation ? [initialLocation?.lat, initialLocation?.lng] : ""}>
-        {#each data.locations as location}
-            <Marker {location}>
-                <Popup />
-            </Marker>
+        {#each locations as category, i}
+            {#each locations[i] as item}
+                {#each item.locations as location}
+                    <Marker location={{name: item.name, ...location}}>
+                        <Popup />
+                    </Marker>
+                {/each}
+            {/each}
         {/each}
     </Map>
 </main>
